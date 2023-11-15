70711f70eee0d02b5a441bb5d525e8531673a100
CyberBoyLTU <91384210+CyberBoyLTU@users.noreply.github.com>
2023-11-15 20:41:33 +0200

    Updated backup and restore functionality
    Backing up defined containers and other improvements in backup process.
    Created functionality to restore containers (all or selected).
    DO NOT TRY TO RESTORE BACKUP AND CADDY CONTAINERS.
    Next will be volumes backup.

--- vhostbackup/server.py
+++ vhostbackup/server.py
@@ -2,6 +2,7 @@
 # 
 # To view backups access: http://vhostbackup.localhost/
 # To create a backup access: http://vhostbackup.localhost/backup
+# To restore a backup access: http://vhostbackup.localhost/restore
 
 # Import all dependencies
 from crypt import methods
@@ -9,6 +10,8 @@ from flask import Flask, request
 from datetime import datetime
 import docker
 import json
+import os
+import tarfile
 
 app = Flask(__name__)
 
@@ -19,103 +22,152 @@ def getDate():
     return current_time
 
 # Return current date and time
-def getNewBackupID():
+def getNewID():
     now = datetime.now()
     newBackupID = now.strftime("%Y%m%d%H%M%S%f")
     return newBackupID
 
 # Checks backup file and returns completed backups
-def getBackupsLog():
+def getBackupsRestoreLogs():
     fileOpened = False
     responseText = ""
 
     # Try reading the file - if file can't be opened it means that it's corrupt and according message is showed
     try:
-        logFile = open("/opt/backup/backupLog.txt")
+        logFile = open("/opt/backup/backupRestoreLog.txt")
         fileOpened = True
     except:
-        responseText = "Oops, no backup logs to show! Don’t worry, you can create a backup and see the log details here afterwards."
+        responseText = "Oops, no backup/restore logs to show! Don’t worry, you can create a backup and see the log details here afterwards."
 
     if(fileOpened):
-        responseText = "Backup logs (showing the status, status reason, ID number and start and end timestamp of each backup):"
+        responseText = "Backup/restore logs (showing type, status, status reason, ID number and start and end timestamp of each backup):"
         for line in logFile:
             # Performs transformation from JSON to more user friendly format
             lineElements = json.loads(line)
             responseText += '<br>'
-            responseText += lineElements["Status"] + " (" + lineElements["StatusReason"] + ") " + lineElements["ID"] + " (" + lineElements["Started"] + " - " + lineElements["Completed"] + ")"
+            responseText += lineElements["Type"] + " | " + lineElements["Status"] + " (" + lineElements["StatusReason"] + ") " + lineElements["ID"] + " (" + lineElements["Started"] + " - " + lineElements["Completed"] + ")"
         logFile.close()
 
     return responseText
 
-# Creates backup log from provided infromation and return completion status      
-def createBackupLog(backupLogData):
+# Creates backup/restore log from provided infromation and return completion status      
+def createBackupRestoreLog(backupRestoreLogData):
     try:
-        backupLog = open('/opt/backup/backupLog.txt' , 'a')
-        backupLog.write(str(backupLogData) + '\n')
+        backupLog = open('/opt/backup/backupRestoreLog.txt' , 'a')
+        backupLog.write(str(backupRestoreLogData) + '\n')
         backupLog.close()
         return True
     except:
         return False
 
+# Creates backup of defined containers
 def backupContainers(backupID, timestamp):
-    # backup tracking variables
-    step0 = True
-    step1 = False
-    step2 = False
-    step3 = False
+    containerList = ["caddy", "vhost1", "vhost2", "vhostbackup"]
+    totalSteps = len(containerList)
+    completedSteps = 0
+    volumepath = "/opt/backup/"
 
-    # Get docker env - step 0
     try:
         client = docker.from_env()
     except:
-        step0 = False
-
-    # backups vhost1 container - step 1
-    try:
-        containerVHOST1 = client.containers.get("vhost1")
-        imageVHOST1 = containerVHOST1.commit(repository="dockerbackup", tag=str(timestamp).replace(" ","").replace("-","").replace(":",""), message=str(backupID))
-        imageVHOST1.save(str(backupID)+"_VHOST1.tar")
-        step1 = True
-    except:
-        step1 = False
-
-    # backups vhost2 container - step 2
-    try:
-        containerVHOST1 = client.containers.get("vhost2")
-        imageVHOST1 = containerVHOST1.commit(repository="dockerbackup", tag=str(timestamp).replace(" ","").replace("-","").replace(":",""), message=str(backupID))
-        imageVHOST1.save(str(backupID)+"_VHOST2.tar")
-        step2 = True
-    except:
-        step2 = False
-
-    # backups vhost1 container - step 3
-    try:
-        containerVHOST1 = client.containers.get("vhostbackup")
-        imageVHOST1 = containerVHOST1.commit(repository="dockerbackup", tag=str(timestamp).replace(" ","").replace("-","").replace(":",""), message=str(backupID))
-        imageVHOST1.save(str(backupID)+"_VHOSTBACKUP.tar")
-        step3 = True
-    except:
-        step3 = False
-
-    #Will need this code in future
-    #To restore a docker container from a backup, you need to use the load_image method, which loads an image from a tar file or a stream. You can specify a repository and a tag for the loaded image, as well as a quiet mode. For example, if you want to load an image from a tar file named “my-backup.tar”, you can do:
-    #image = client.images.load_image("my-backup.tar", repository="my-backup", tag="latest", quiet=False)
-    #The load_image method returns an image object, which you can use to inspect or manipulate the image. For example, you can print the image ID or run a container from the image:
-    #container = client.containers.run(image, name="my-restored-container", detach=True)
-    #The run method returns a container object, which you can use to inspect or manipulate the container. For example, you can print the container ID or start the container:
-    #container.start()
-
-    if (step0 and step1 and step2 and step3):
-        return True
-    else:
         return False
 
+    for name in containerList:
+        try:
+            container = client.containers.get(name)
+            tag = str(timestamp).replace(" ", "").replace("-", "").replace(":", "")
+            image = container.commit(repository="dockerbackup_"+name, tag=tag, message=str(backupID))
+            with open(os.path.join(volumepath, f"{backupID}_{name.upper()}.tar"), 'wb') as f:
+                for chunk in image.save(chunk_size=2048):
+                    f.write(chunk)
+            completedSteps += 1
+        except:
+            return False
+
+    return completedSteps == totalSteps
+
+# Restores a specified container from backup
+def restoreContainer(backupid, timestamp, name):
+    volumepath = "/opt/backup/"
+
+    try:
+        client = docker.from_env()
+    except:
+        return False
+
+    try:
+        with open(os.path.join(volumepath, f"{backupid}_{name.upper()}.tar"), 'rb') as f:
+            client.images.load(f.read())
+        if name in [container.name for container in client.containers.list()]:
+            container = client.containers.get(name)
+            container.stop()
+            container.remove()
+        tag = str(timestamp).replace(" ", "").replace("-", "").replace(":", "")
+        imageName = "dockerbackup_"+name+":"+tag
+        container = client.containers.run(imageName, detach=True, name=name)
+        return True
+    except:
+        return False
+
+# Performs all restoration process steps (performs restore and logs actions)
+def restoreBackup():
+    # Set starting variables
+    startTimestamp = getDate()
+    restoreID = str(getNewID())
+    status = "STARTED"
+    statusReason = ""
+    statusReasonAdditional = ""
+    statusNoErrors = True
+    
+    restoreContainer("", "", "vhost1")
+
+    # Runs all backup codes
+    # Calls ontainer backup code and logs status of it
+    restoreBackupID = "20231115183718429291"
+    restoreBackupTimestamp = "2023-11-15 18:37:18"
+    restoreContainerList = ["vhost1", "vhost2"]
+
+    for name in restoreContainerList:
+        containerRestoreDone = restoreContainer(restoreBackupID, restoreBackupTimestamp, name)
+        if (containerRestoreDone == False):
+            statusReasonAdditional += "FAILED: Container " + name + " restore;"
+            statusNoErrors = False
+
+    # check if backups completed and set status
+    if(statusNoErrors):
+        status = "COMPLETED"
+        statusReason = "DONE: 1/1"
+    else:
+        status = "FAILED"
+        statusReason = "DONE: 0/1; " + statusReasonAdditional
+
+
+    # building response and log information
+    backupResponseBuild = {
+        "ID": str(restoreID),
+        "Type": "RESTORE (" + restoreBackupID + ")",
+        "Status": str(status),
+        "StatusReason": str(statusReason),
+        "Started": str(startTimestamp),
+        "Completed": str(getDate())
+    }
+    backupResponse = json.dumps(backupResponseBuild)
+    
+    # Logs backup
+    if (createBackupRestoreLog(backupResponse)):
+        return backupResponse
+    else:
+        # In case we get an error while saving a log a warning message is return with a reason to the caller
+        backupResponseBuild["Status"] = "WARNING"
+        backupResponseBuild["StatusReason"] = "Failed to create backup log"
+        backupResponse = json.dumps(backupResponseBuild)
+        return backupResponse
 
 # Performs all backup process steps (creates backup, logs actions)
 def createBackup():
     # Set starting variables
     startTimestamp = getDate()
-    backupID = str(getNewBackupID())
+    backupID = str(getNewID())
     status = "STARTED"
     statusReason = ""
     
@@ -136,6 +188,7 @@ def createBackup():
     # building response and log information
     backupResponseBuild = {
         "ID": str(backupID),
+        "Type": "BACKUP",
         "Status": str(status),
         "StatusReason": str(statusReason),
         "Started": str(startTimestamp),
@@ -144,7 +197,7 @@ def createBackup():
     backupResponse = json.dumps(backupResponseBuild)
     
     # Logs backup
-    if (createBackupLog(backupResponse)):
+    if (createBackupRestoreLog(backupResponse)):
         return backupResponse
     else:
         # In case we get an error while saving a log a warning message is return with a reason to the caller
@@ -157,7 +210,7 @@ def createBackup():
 @app.route('/', methods = ['GET'])
 def mainViewFunction():
     if(request.method == 'GET'):
-        return getBackupsLog()
+        return getBackupsRestoreLogs()
 
 # This code is executed once http://vhostbackup.localhost/backup is called
 @app.route('/backup', methods = ['GET'])
@@ -170,7 +223,20 @@ def backupNow():
         #responseStr = response["ID"] + " " + response["Status"] + " " + response["Started"] + " - " + response["Completed"]
 
         return createBackupResponse
-        
+
+# This code is executed once http://vhostbackup.localhost/restore is called
+@app.route('/restore', methods = ['GET'])
+def restoreNow():
+    if(request.method == 'GET'):
+        retoreBackupResponse = restoreBackup()
+
+        # Other code option for nice display
+        #response = json.loads(createBackupResponse)
+        #responseStr = response["ID"] + " " + response["Status"] + " " + response["Started"] + " - " + response["Completed"]
+
+        return retoreBackupResponse
+
+
 # Still I have to figure out what this does :)
 if __name__ == '__main__':
 	app.run(debug=False, host='0.0.0.0', port=80)
