ce360e8196255eb2825b0fd5e68c5419eafbca69
Jonas Girdzijauskas <jonas.girdzijauskas@icloud.com>
2023-11-24 16:34:11 +0200

    initial fe+api implementation

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,18 @@
 import { type FC, type ChangeEvent, useState, useEffect } from "react";
-import axios from "axios";
 
 import type { TDocument } from "./types";
+import { getDocuments, getCredits } from "./utils";
+import { getFormattedSize } from "./utils/getFormattedSize";
 
 // TODO: fronta +-susibuildint, palikt nginx+react front-end kaip atskira ir prisidet dar backend expresiuka su s3 upload endpointu
 // export/docker env var AWS auth -> leis naudot aws-sdk -> expresse s3 upload endpointai -> fronta subuildint ir delete react dir kad tiesiog axiosas butu ir rodytu updated + delete mygtuka
 // TODO: nginx 404 refresh fix
+// TODO: vu docker: data storage, virtual segregation networks
+// TODO: translations simple example
 export const App: FC = () => {
-  const [documents, setDocuments] = useState<any[]>([]);
-  const [selectedDocuments, setSelectedDocuments] = useState<TDocument[]>([]);
+  const [_documents, setDocuments] = useState<any[]>([]);
+  const [_selectedDocuments, setSelectedDocuments] = useState<TDocument[]>([]);
+  const _credits = getCredits();
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newSelectedFiles = Array.from(event.target.files || []);
@@ -21,19 +25,8 @@ export const App: FC = () => {
 
   useEffect(() => {
     // TODO: nginx reverse to use http://api https://stackoverflow.com/a/77060234
-
-    // js runs in the browser, so it can't access the docker socket https://stackoverflow.com/a/56375180
-    axios
-      .get("http://localhost:3000/api/v1/documents")
-      .then((res) => {
-        if (Array.isArray(res.data.documents)) {
-          setDocuments(res.data.documents);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+    getDocuments().then(setDocuments);
+  }, [getDocuments]);
 
   return (
     <div
@@ -53,52 +46,24 @@ export const App: FC = () => {
         </h2>
 
         <input
-          style={{
-            fontSize: "1.5rem",
-            padding: "1rem 2rem",
-            marginBottom: "6rem",
-            textAlign: "center",
-            margin: "0 auto",
-            display: "flex",
-            justifyContent: "center",
-            boxShadow: "0px 12px 22px 1px rgba(0, 0,100, .07)",
-            borderRadius: "6px",
-          }}
           onChange={handleInputChange}
+          id="file-upload-input"
           type="file"
           multiple
         />
 
         <section id="documents-list">
-          {selectedDocuments.map((document, index) => (
-            <div
-              style={{
-                backgroundColor: "rgba(0, 0, 0, .03)",
-                padding: "2rem 1rem",
-                width: "80vw",
-                textAlign: "center",
-                margin: "0 auto",
-                alignContent: "center",
-                alignItems: "center",
-                alignSelf: "center",
-                marginBottom: "1rem",
-                borderRadius: "32px",
-                display: "flex",
-              }}
-              key={index}
-            >
+          {_selectedDocuments.map((document, index) => (
+            <div id="selected-upload-document-container" key={index}>
               <p className="document-name">{document.name}&nbsp;</p>
-
-              <p className="document-size">
-                <i>{(document.size / 1024 / 1024).toFixed(1)}&nbsp;MB</i>
-              </p>
+              <p className="document-size">{getFormattedSize(document.size)}</p>
             </div>
           ))}
         </section>
 
         {/* TODO: MUI Grid  */}
         <div id="all-documents-container">
-          {documents.map((document) => (
+          {_documents.map((document) => (
             <div id="document-container" key={document.name + document.url}>
               <p className="bold">{document.name}</p>
               <p className="fetched-document-size">{document.size}B</p>
@@ -109,8 +74,7 @@ export const App: FC = () => {
 
       <footer>
         <p id="footer-credits" aria-label="file sharing site credits">
-          © {new Date().getFullYear()}&nbsp; Jonas Girdzijauskas, Akvilė
-          Mickevičūtė, Tadas Kastanauskas, Renata Marcinauskaitė
+          {_credits}
         </p>
       </footer>
     </div>
